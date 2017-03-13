9cd6a4b41e501a8088d71dd1f73956b432336257
Joshua Jackson <washingtonfaulconer@gmail.com>
2017-03-13 11:49:40 +0000

    Enemy and Player changes.
    Implemented the enemy movement. Will move randomly between any amount of
    points you give it. It is all modular and so points can be removed and
    added and the enemy should adapt accordingly. Created the intial player
    script, doesn't control any movement but the player has a certain amount
    of lives and upon collision with an enemy, will lose one of those lives
    (or more than 1 if a higher value is  assigned to an enemy). When an
    enemy hits a player, it will also play a sound that simulates the player
    being hit. Finally via a Death() function, when the player has lost
    their maximum amount of lives, a message is outputted to the console to
    indicate as such and a boolean isDead is set to true. Someone else who
    handles what happens after their death (I think Tom) will act according
    to the value of isDead and so on.

---
+++ Grumble - The Cleanup/Assets/Scripts/EnemyMangement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMangement : MonoBehaviour
+{
+    //Components
+    [SerializeField]
+    private Transform[] movementPoints; //The points which the enemy will move between at random.
+    private Transform trans;            //The enemy's transform.
+    [SerializeField]
+    private AudioSource playerHitSound; //The audio source containing the player hit sound. Assigned in inspector.
+
+    //Attributes
+    [SerializeField]
+    private float movementSpeed;    //The speed at which the enemy will move in Unity Units. Assigned in the inspector.
+    private bool hasMoved = true;   //The check to see if the enemy is at its target point. (See Movement()).
+    private int r;                  //The random number generated to choose a point to move to. (See Movement()).
+    [SerializeField]
+    private int dmg = 1;           //The enemy's damage, measured in lives it takes.
+
+    void Start()
+    {
+        trans = GetComponent<Transform>();
+    }
+    void Update()
+    {
+        Movement();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            playerHitSound.Play();  //Plays the playerHitSound assigned in the inspector on the Enemy object.
+            PlayerController.TakeDamage(dmg);   //If this enemy hits a player. Call the Player's TakeDamage() function and pass in the damage this enemy does.
+        }
+    }
+
+    /*Move() will move the enemy object it is attached to, to random points within the movementPoints array (assigned in inspector)
+    Once it reaches a point, it will choose another point at random and repeat.*/
+    void Movement()
+    {
+        /*Checks to see if the enemy has reached its first point. Intially set to true to make sure
+        that a random point is chosen at least once before the enemy has moved.*/
+        if (hasMoved)
+        {
+             r = Random.Range(0, movementPoints.Length);
+        }
+
+        float timeStep = movementSpeed * Time.deltaTime;    //Smoothing out the movementSpeed by the time passed in the previous frame.
+        trans.position = Vector2.MoveTowards(trans.position, movementPoints[r].position, timeStep); //Moving the object towards the movementPoint.
+        //Debug.Log("Enemy Moving to point " + movementPoints[r].position);
+
+        /*If the enemy has reached its point, sets hasMoved to true to make sure another
+        random point can be chosen. If it has not yet reached its point, it sets hasMoved
+        to false to avoid a new random point being chosen before it has finished moving.*/
+        if (trans.position == movementPoints[r].position)
+        {
+            hasMoved = true;
+        }
+        else
+        {
+            hasMoved = false;
+        }
+    }
+}
---
+++ Grumble - The Cleanup/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*This is the script that manages the player. All work on the player goes inside of here.*/
+public class PlayerController : MonoBehaviour
+{
+    //Components
+    private Transform trans;
+
+    //Attributes
+    private static int lives = 5;
+    private bool isDead = false;
+
+	void Start ()
+    {
+        trans = GetComponent<Transform>();  //Grabs the players transform and assigns it locally. (Less intensive than grabbing it everytime).
+	}
+	void Update ()
+    {
+        Death();
+	}
+
+    void Death()
+    {
+        if (lives <= 0)
+        {
+            Debug.Log("Player has died..");
+            isDead = true;
+        }
+    }
+
+    /*Called when a player is hit by an enemy. Will reduce the player's lives.*/
+    public static void TakeDamage(int livesToTake)
+    {
+        lives -= livesToTake;   //livesToTake is equal to the damage passed in when this function is called. (via the EnemyManagement.cs script.)
+        Debug.Log("Player has lost " + livesToTake + " live[s]");
+    }
+
+}
