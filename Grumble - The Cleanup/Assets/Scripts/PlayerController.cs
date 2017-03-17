6dec951682212d4eecdd38c589ebeee05826d21f
Alex <mealexmabe@hotmail.co.uk>
2017-03-17 23:29:24 +0000

    Player/Enemy Edits - Read Description
    + Attack Animation
    + Enemy Health System
    + Enemy Death System
    + Enemy Death Particles
    + Player Cannot be attacked whilst attacking
    + Player Attack Sound
    + Enemy Hurt Sound
    + Camera Following Player
    + Player Prefab
    + Death Particle Prefab
    + Temp-enemy/platform Prefab
    + Leftover Particle Destroying Script
    + Fixed Player Walking Animation (previously in wrong order)
    + Stopped Movement And Attacking at same time
    + Gave lives to Enemy
    + Moved attack sprites into one .PNG
    + Added Audio sources to Player/Enemy
    + Removed Enemies from game when they Die
    + When player attacks their Box Collider Increases
    + Linked controls to Input pref

--- Grumble - The Cleanup/Assets/Scripts/PlayerController.cs
+++ Grumble - The Cleanup/Assets/Scripts/PlayerController.cs
@@ -7,32 +7,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     //Components
-    //private Transform trans;
+    private Transform trans;
+
 	private Rigidbody2D myRigidbody;
 	private bool facingRight;
 
-	private Animator myAnimator;
+	private Animator myAnimator; //animation
 
 	[SerializeField]
-	private float movementSpeed;
-	public Vector2 jumpHeight;
+	private float movementSpeed;//movement
+	public float jumpHeight;	//jump height
 
 	public Transform groundCheck;
-	public float groundCheckRadius;
+	public float groundCheckRadius;	//ground check
 	public LayerMask whatIsGround;
 	private bool grounded;
 	private bool Jump = false;
 
+	private bool attack;
+	private BoxCollider2D BCollider;
+	private float Box_X;
+	private float Box_Y;
+
+	[SerializeField]
+	private int Dmg = 1;
+	[SerializeField]
+	private AudioSource EnemyHitSound;
+	[SerializeField]
+	private AudioSource EnemyPainSound;
+
+
+
     //Attributes
+	[SerializeField]
    private static int lives = 5;
    private bool isDead = false;
 
 	void Start ()
     {
-       //trans = GetComponent<Transform>();  //Grabs the players transform and assigns it locally. (Less intensive than grabbing it everytime).
+       trans = GetComponent<Transform>();  //Grabs the players transform and assigns it locally. (Less intensive than grabbing it everytime).
 
 		myRigidbody = GetComponent<Rigidbody2D> ();
 		myAnimator = GetComponent<Animator> ();
+		BCollider = GetComponent<BoxCollider2D> ();
+		Box_X = BCollider.size.x;
+		Box_Y = BCollider.size.y;
 	}
 		
 
@@ -47,26 +66,27 @@ public class PlayerController : MonoBehaviour
     {
 		float horizontal = Input.GetAxis ("Horizontal");
 		HandleMovement(horizontal);
-
+		HandleInput ();
+		HandleAttacks ();
 		Flip (horizontal);
-
-
-
-
-
+		ResetValues ();
         Death();
 	}
 
 	private void HandleMovement(float horizontal)
 	{
-		myRigidbody.velocity = new Vector2 (horizontal * movementSpeed, myRigidbody.velocity.y);
 
-		myAnimator.SetFloat ("LeftWalk", Mathf.Abs(horizontal));
+		if (!this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+		{
+			myRigidbody.velocity = new Vector2 (horizontal * movementSpeed, myRigidbody.velocity.y);
+		}
+
+		myAnimator.SetFloat ("Walking", Mathf.Abs(horizontal));
 
 
 		if (Input.GetButton ("Jump") && grounded)
 		{
-			GetComponent<Rigidbody2D> ().AddForce (jumpHeight, ForceMode2D.Impulse);
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2(0, jumpHeight), ForceMode2D.Impulse);
 		}
 	}
 
@@ -83,6 +103,57 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 
+	private void HandleAttacks()
+	{
+		if (attack && !this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+		{
+			myAnimator.SetTrigger ("Attack");
+			EnemyHitSound.Play ();
+			myRigidbody.velocity = Vector2.zero;
+		}
+
+		if (this.myAnimator.GetCurrentAnimatorStateInfo (0).IsTag ("Attack")) 
+		{
+			Vector2 b_size = BCollider.size;
+			b_size = new Vector2 (6, Box_Y);
+			BCollider.size = b_size;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Enemy") 
+		{
+			
+			if (this.myAnimator.GetCurrentAnimatorStateInfo (0).IsTag ("Attack")) 
+			{
+				EnemyPainSound.Play ();
+				EnemyMangement.TakeDamage (Dmg);
+			}
+		}
+	}
+
+	private void HandleInput()
+	{
+		if (Input.GetButtonDown ("Attacking"))
+		{
+			attack = true;
+		}
+	}
+
+
+
+	private void ResetValues()
+	{
+		attack = false;
+
+		if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag ("NotAttack")) 
+		{
+			Vector2 b_size = BCollider.size;
+			b_size = new Vector2 (Box_X, Box_Y);
+			BCollider.size = b_size;
+		}
+	}
 
 
 
@@ -101,8 +172,12 @@ public class PlayerController : MonoBehaviour
     /*Called when a player is hit by an enemy. Will reduce the player's lives.*/
     public static void TakeDamage(int livesToTake)
     {
-        lives -= livesToTake;   //livesToTake is equal to the damage passed in when this function is called. (via the EnemyManagement.cs script.)
-        Debug.Log("Player has lost " + livesToTake + " live[s]");
+		
+		if (!GameObject.Find("Player").GetComponent<PlayerController>().attack) 
+		{
+			lives -= livesToTake;   //livesToTake is equal to the damage passed in when this function is called. (via the EnemyManagement.cs script.)
+			Debug.Log ("Player has lost " + livesToTake + " live[s]");
+		}
     }
 
 }
