2796c8f1febace576b36896a5424b6ac0731a7b5
KirkimusII <kirk2242@gmail.com>
2017-04-03 22:18:04 +0100

    Grumble counter script and score things

---
+++ Grumble - The Cleanup/Assets/Scripts/GumbleCounter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class GumbleCounter : MonoBehaviour
+{
+
+    public int score;
+    public Text ScoreText;
+    [SerializeField]
+    private int ScoreValue;
+    [SerializeField]
+
+    // Use this for initialization
+    void Start()
+    {
+        ScoreValue = 0;
+        UpdateScoreText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Function to update the score and display it on the UI
+    public void UpdateScoreValue(int scoreUpdate)
+    {
+        ScoreValue += scoreUpdate;
+
+        UpdateScoreText();
+    }
+
+    //Shows the updated score in game.
+    void UpdateScoreText()
+    {
+        ScoreText.text = "Grumble Counter: " + ScoreValue;
+    }
+
+    //Resets the score
+    void ResetScore()
+    {
+        ScoreValue = 0;
+        UpdateScoreText();
+    }
+
+}
